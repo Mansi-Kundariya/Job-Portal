aa87f024931674183b2f222477ec88d77ff2b2be
Mansi-Kundariya <mansi.kundariya2612@gmail.com>
2024-09-18 15:19:36 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Database connected to successfully');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB Atlas:', err);
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Server started on port');
